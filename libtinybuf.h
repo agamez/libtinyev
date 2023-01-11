40ec1af96d2fff002e56863e063879fe19e7b83a
Alvaro Gamez Machado <alvaro.gamez@hazent.com>
2023-01-11 10:10:20 +0100

    Add missing libtinybuf.h

---
+++ libtinybuf.h
@@ -0,0 +1,27 @@
+#ifndef __libtinybuf_h__
+#define __libtinybuf_h__
+
+#include <inttypes.h>
+#include "libtinyev.h"
+
+struct ltiny_event_buf;
+
+/**
+ * @brief Event callback. The user will write one or more functions with this prototype and pass them to the library when registering an event.
+ * @param[in] ctx ltiny_ev context
+ * @param[in] ev ltiny_ev event that triggered the callback
+ * @param[in] data Data received via file descriptor
+ * @param[in] data_length Size of the data received
+ *
+ * Whenever the event happens, this user provided function will be called
+ */
+typedef void (*ltiny_event_buf_cb)(struct ltiny_ev_ctx *ctx, struct ltiny_event_buf *ev_buf, void *data, size_t count);
+
+void *ltiny_event_buf_consume(struct ltiny_ev_ctx *ctx, struct ltiny_event_buf *ev_buf, size_t count);
+
+void *ltiny_evbuf_get_user_data(struct ltiny_event_buf *ev_buf);
+struct ltiny_event_buf *ltiny_ev_new_buf_event(struct ltiny_ev_ctx *ctx, int fd, ltiny_event_buf_cb callback, void *user_data);
+int ltiny_event_buf_send(struct ltiny_ev_ctx *ctx, struct ltiny_event_buf *ev_buf, void *buf, size_t count);
+
+
+#endif /* __libtinybuf_h__ */
